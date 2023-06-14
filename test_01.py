75c78959116eb089a5914d352fc285d733852694
flavio <flavioborinjunior@gmail.com>
2023-06-14 13:38:50 -0300

    Testa se o botão funciona

--- test_01.py
+++ test_01.py
@@ -2,6 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 @pytest.fixture
 def setup():
@@ -20,6 +22,12 @@ def test_title(setup):
 def test_swap(setup):
     driver = setup
     sleep(2)
-    rButton = driver.find_element(By.XPATH, "//a[contains (@class, 'bx-prev uzk15__iconlink')]")
-    rButton.click()
-    assert 1 == 1
+    lButton = driver.find_element(By.XPATH, "//a[contains (@class, 'bx-prev uzk15__iconlink')]")
+    img = driver.find_element(By.XPATH, "//*[@id=\"mainslider1\"]/div[3]/div[1]/ul")
+    style1 = img.get_attribute("style")
+    lButton.click()
+    sleep(2)
+    style2 = img.get_attribute("style")
+    print(style1)
+    print(style2)
+    assert style1 != style2
