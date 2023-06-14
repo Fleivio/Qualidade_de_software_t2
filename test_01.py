17f9777739e6c2c993f7be7b6d0273f0e02d47c8
flavio <flavioborinjunior@gmail.com>
2023-06-14 10:52:41 -0300

    Acesso a botao

--- test_01.py
+++ test_01.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
 @pytest.fixture
 def setup():
@@ -14,4 +16,10 @@ def test_title(setup):
     title = driver.title
     assert "Universität zu Köln" in title
 
-    
+@pytest.mark.test
+def test_swap(setup):
+    driver = setup
+    sleep(2)
+    rButton = driver.find_element(By.XPATH, "//a[contains (@class, 'bx-prev uzk15__iconlink')]")
+    rButton.click()
+    assert 1 == 1
