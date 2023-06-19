9c191148ee04efaf0ca25cc9a3dd9d49d357d467
flavio <flavioborinjunior@gmail.com>
2023-06-19 19:20:09 -0300

    Mais testes, melhorara

--- test_01.py
+++ test_01.py
@@ -1,9 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 @pytest.fixture
 def setup():
@@ -31,3 +35,69 @@ def test_swap(setup):
 
     style2 = img.get_attribute("style")
     assert style1 != style2
+
+@pytest.mark.test
+def test_header_button(setup):
+    driver = setup
+    button = driver.find_element(By.XPATH, "//html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")
+    dropDown = driver.find_element(By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div")
+    sleep(2)
+    button.click()
+    sleep(2)
+
+    hidden = dropDown.get_attribute("aria-hidden")
+
+    assert hidden == "false"
+
+
+@pytest.mark.test
+def test_header_button_link(setup):
+    driver = setup
+    button = driver.find_element(By.XPATH, "//html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")
+    button.click()
+    sleep(2)
+
+    buttonLink = driver.find_element(By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div/ul/li[3]/ul/li[1]/a")
+    buttonLink.click()
+
+    sleep(2)
+    title = driver.title
+    assert title in "Rektor"
+
+@pytest.mark.test
+def test_links_span(setup):
+    driver = setup
+
+    element = driver.find_element(By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p")
+    element.click()
+
+    childs =  driver.find_element(By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p/div")
+
+    assert childs.is_displayed()
+
+@pytest.mark.test
+def test_dropdown_footer(setup):
+    driver = setup
+    sleep(2)
+    button = driver.find_element(By.XPATH, "//*[@id=\"footertrigger\"]")
+    button.click()
+    sleep(2)
+
+    dropdown = driver.find_element(By.XPATH, "//*[@id=\"footerbox\"]")
+    assert dropdown.is_displayed()
+
+@pytest.mark.test
+def test_to_top(setup):
+    driver = setup
+
+    defScrl = driver.execute_script("return document.body.scrollHeight")
+
+    html = driver.find_element(By.XPATH, "/html")
+    html.send_keys(Keys.END)
+
+    sleep(2)
+    
+    toTopButton = driver.find_element(By.XPATH, "/html/body/header/div/div[1]/div/div/div[2]/a")
+    toTopButton.click()
+    sleep(2)
+    assert driver.execute_script("return document.body.scrollHeight") == defScrl
