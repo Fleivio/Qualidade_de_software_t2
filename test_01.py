be9391413ea72b161ba5e610b45ea9f36d25740d
flavio <flavioborinjunior@gmail.com>
2023-06-26 21:55:35 -0300

    adicionados waits

--- test_01.py
+++ test_01.py
@@ -28,38 +28,37 @@ def test_title(setup):
 def test_swap(setup):
     driver = setup
 
-    rButton = driver.find_element(By.XPATH, "//*[@id=\"mainslider1\"]/div[2]/div/span[2]/a")
-    style1 = rButton.get_attribute("class")
-    rButton.click()
-    sleep(2)
+    sleep(1)
 
-    style2 = rButton.get_attribute("class")
+    slider = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/div[1]/div[1]/section/div[3]/div[1]/ul")))
+    style1 = slider.get_attribute("style")
+    
+    rButton = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/div[1]/div[1]/section/div[2]/div/span[2]/a")))
+    rButton.click()
+    sleep(1)
+    style2 = slider.get_attribute("style")
     assert style1 != style2
 
 
 @pytest.mark.test
 def test_header_button(setup):
     driver = setup
-    button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")))
-    dropDown = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div")))
-    sleep(2)
+    button = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")))
     button.click()
-    sleep(2)
 
+    dropDown = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div")))
     assert dropDown.is_displayed()
 
 
 @pytest.mark.test
 def test_header_button_link(setup):
     driver = setup
-    button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")))
+    button = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//html/body/header/div/div[3]/div/div/nav/ul/li[1]/a")))
     button.click()
-    sleep(2)
 
-    buttonLink = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div/ul/li[3]/ul/li[1]/a")))
+    buttonLink = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[3]/div/div/nav/ul/li[1]/div/ul/li[3]/ul/li[1]/a")))
     buttonLink.click()
 
-    sleep(2)
     title = driver.title
     assert title in "Rektor"
 
@@ -68,22 +67,20 @@ def test_header_button_link(setup):
 def test_links_span(setup):
     driver = setup
 
-    element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p")))
+    element = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p")))
     element.click()
 
-    childs =  WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p/div")))
-    sleep(2)
+    childs =  WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[2]/p/div")))
     assert childs.is_displayed()
 
 @pytest.mark.test
 def test_dropdown_footer(setup):
     driver = setup
 
-    button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"footertrigger\"]")))
+    button = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"footertrigger\"]")))
     button.click()
-    sleep(2)
 
-    dropdown = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"footerbox\"]")))
+    dropdown = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"footerbox\"]")))
     assert dropdown.is_displayed()
 
 @pytest.mark.test
@@ -94,22 +91,19 @@ def test_to_top(setup):
 
     html = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html")))
     html.send_keys(Keys.END)
-
-    sleep(2)
     
-    toTopButton = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[1]/div/div/div[2]/a")))
+    toTopButton = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[1]/div/div/div[2]/a")))
     toTopButton.click()
-    sleep(7)
+
     assert driver.execute_script("return document.body.scrollHeight") == defScrl
 
 @pytest.mark.test
 def test_search(setup):
     driver = setup
 
-    search = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[3]/input[1]")))
+    search = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/div[2]/form[3]/input[1]")))
     search.send_keys("test")
     search.send_keys(Keys.ENTER)
-    sleep(2)
 
     assert driver.title == "Google-Suche"
 
@@ -117,13 +111,11 @@ def test_search(setup):
 def test_language_switch(setup):
     driver = setup
 
-    button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/nav/div/form/p/span/select")))
+    button = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[2]/div/div/nav/div/form/p/span/select")))
     button.click()
-    sleep(2)
 
-    dropdown = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/header/div/div[2]/div/div/nav/div/form/p/div")))
+    dropdown = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/div[2]/div/div/nav/div/form/p/div")))
     dropdown.click()
-    sleep(2)
 
     lang = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html"))).get_attribute("lang")
 
@@ -133,10 +125,9 @@ def test_language_switch(setup):
 def test_hover(setup):
     driver = setup
 
-    button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/article[2]/a")))
+    button = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/article[2]/a")))
     hover = ActionChains(driver).move_to_element(button)
     hover.perform()
-    sleep(2)
 
     assert button.value_of_css_property("background-color") == "rgba(50, 71, 91, 1)"
 
@@ -145,7 +136,7 @@ def test_hover(setup):
 def test_image_alt(setup):
     driver = setup
 
-    img = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/article[2]/a/figure/img")))
+    img = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/article[2]/a/figure/img")))
     alt = img.get_attribute("alt")
 
     assert alt == "Gebäude aus der Vogelperspektive"
@@ -153,78 +144,58 @@ def test_image_alt(setup):
 @pytest.mark.test
 def test_pagination(setup):
     driver = setup
-
-    link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[12]/span/a")))
+    sleep(2)
+    link = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/main/div[12]/span/a")))
     link.click()
-    sleep(2)
-    link_4 = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[8]/div/section/ul[1]/li[4]/a")))
+
+    link_4 = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/main/div[8]/div/section/ul[1]/li[4]/a")))
     link_4.click()
-    sleep(2)
-    link_4_father = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[8]/div/section/ul[1]/li[5]")))
+
+    link_4_father = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[8]/div/section/ul[1]/li[5]")))
 
     assert "current" in link_4_father.get_attribute("class")
 
 @pytest.mark.test
 def test_checkbox(setup):
     driver = setup
-    
-    link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
-    link.click()
-    sleep(3)
-    link_drop = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend")))
-    link_drop.click()
-    sleep(3)
-    link_checkbox = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/div/div[1]/input")))
-    link_checkbox.click()
     sleep(2)
+    link = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
+    link.click()
+
+    link_drop = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend")))
+    link_drop.click()
+
+    link_checkbox = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/div/div[1]/input")))
+    link_checkbox.click()
 
     assert link_checkbox.get_attribute("checked") 
 
 @pytest.mark.test
 def test_checked_count(setup):
     driver = setup
-
-    link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
+    sleep(2)
+    link = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
     link.click()
 
-    link_drop = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend")))
-    sleep(1)
+    link_drop = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend")))
     link_drop.click()
 
-    link_checkbox = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/div/div[1]/input")))
-    sleep(1)
+    link_checkbox = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/div/div[1]/input")))
     link_checkbox.click()
 
-    box_count = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend/span")))
+    box_count = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend/span")))
     assert box_count.text == "1"
 
-
-@pytest.mark.test
-def test_checked_c(setup):
-    driver = setup
-
-    link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
-    sleep(1)
-    link.click()
-
-    link_drop = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend")))
-    link_drop.click()
-    sleep(2)
-    link_checkbox = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/div/div[1]/input")))
-    link_checkbox.click()
-    sleep(2)
-    box_count = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[2]/div[3]/div/fieldset/legend/span")))
-    assert box_count.text == "1"
 
 @pytest.mark.test
 def test_first_letter_selection(setup):
     driver = setup
-
-    link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
-    link.click()
     sleep(2)
-    link_letter = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[1]/div/nav/ul/li[1]/a")))
+    link = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[3]/div/div/div[2]/div[1]/ul/li[1]/a")))
+    link.click()
+
+    link_letter = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/aside/div[2]/div[5]/div/form[1]/div/nav/ul/li[1]/a")))
     link_letter.click()
-    sleep(10)
-    first_course = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[1]/div[5]/div/ul/li/article/a/h2/em")))
+
+    first_course = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/main/div[1]/div[5]/div/ul/li/article/a/h2/em")))
     assert first_course.text[0] == "A"
